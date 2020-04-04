99750ad7e843fa22b013f26a25a7634daef610ad
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-03 22:16:42 -0300

    EacCli::Runner::PositionalArgument.

---
+++ lib/eac_cli/runner/positional_argument.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EacCli
+  module Runner
+    class PositionalArgument
+      common_constructor :name
+    end
+  end
+end
