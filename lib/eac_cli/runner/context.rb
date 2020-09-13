b2643d094e06c58f80fa18ae5bd35639597b8cf3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-09-13 12:48:50 -0300

    EacCli::Runner::Context.

---
+++ lib/eac_cli/runner/context.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EacCli
+  module Runner
+    class Context
+      attr_reader :argv, :parent, :program_name
+
+      def initialize(*context_args)
+        options = context_args.extract_options!
+        @argv = (context_args[0] || options.delete(:argv)).freeze
+        @parent = context_args[1] || options.delete(:parent)
+        @program_name = options.delete(:program_name)
+      end
+    end
+  end
+end
