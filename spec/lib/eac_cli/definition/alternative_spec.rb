ffb89f2f476e62e16102ee8bd7bbad646298d174
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-21 20:05:30 -0300

    EacCli::Definition::Alternative.

---
+++ spec/lib/eac_cli/definition/alternative_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'eac_cli/definition/alternative'
+
+RSpec.describe ::EacCli::Definition::Alternative do
+  let(:instance) { described_class.new }
+
+  it { expect { instance.arg_opt '-a', '--opt2', 'A argument option' }.not_to raise_error }
+  it { expect { instance.bool_opt '-b', '--opt1', 'A boolean option' }.not_to raise_error }
+  it { expect { instance.options_argument(true) }.not_to raise_error }
+  it { expect { instance.pos_arg :pos1 }.not_to raise_error }
+  it { expect { instance.pos_arg :pos2, optional: true, repeat: true }.not_to raise_error }
+  it { expect { instance.subcommands }.not_to raise_error }
+end
