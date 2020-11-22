02244919aff4dcb669775c2f0a6b21dd947f34ec
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-22 18:20:52 -0300

    EacCli::Parser::ParseResult: absorbed by EacCli::Parser.

--- lib/eac_cli/parser.rb
+++ lib/eac_cli/parser.rb
@@ -8,7 +8,29 @@ module EacCli
     common_constructor :definition
 
     def parse(argv)
-      ::EacCli::Parser::ParseResult.new(definition, argv).result
+      result_or_error = parse_definition(definition, argv)
+      return result_or_error unless result_or_error.is_a?(::Exception)
+
+      definition.alternatives.each do |alternative|
+        alt_result_or_error = parse_definition(alternative, argv)
+        return alt_result_or_error unless alt_result_or_error.is_a?(::Exception)
+      end
+
+      raise result_or_error
+    end
+
+    private
+
+    def parse_definition(definition, argv)
+      ::EacCli::Parser::Collector.to_data(definition) do |collector|
+        ::EacCli::Parser::PositionalCollection.new(
+          definition,
+          ::EacCli::Parser::OptionsCollection.new(definition, argv, collector).arguments,
+          collector
+        )
+      end
+    rescue ::EacCli::Parser::Error => e
+      e
     end
   end
 end
