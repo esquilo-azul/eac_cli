8d7eb7a3209b017db7dffdba516c3d06a1c81c94
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-04 12:32:19 -0300

    EacCli::RunnerWith::OutputFile.output_content: abstract method.

--- lib/eac_cli/runner_with/output_file.rb
+++ lib/eac_cli/runner_with/output_file.rb
@@ -2,12 +2,16 @@
 
 require 'eac_cli/runner'
 require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/abstract_methods'
 
 module EacCli
   module RunnerWith
     module OutputFile
       common_concern do
         include ::EacCli::Runner
+        include ::EacRubyUtils::AbstractMethods
+
+        abstract_methods :output_content
 
         runner_definition do
           arg_opt '-o', '--output-file', 'Output to file.'
