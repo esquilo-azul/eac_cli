c8bad181fa4dd427bb67cd6df40e616e9e69d7f9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-02-04 19:56:17 -0300

    EacCli::Runner::InstanceMethods: move to self file.

--- lib/eac_cli/runner.rb
+++ lib/eac_cli/runner.rb
@@ -47,29 +47,5 @@ module EacCli
       include ActiveSupport::Callbacks
       define_callbacks :run
     end
-
-    module InstanceMethods
-      def run_run
-        parsed
-        run_callbacks(:run) { run }
-      rescue ::EacCli::Runner::Exit # rubocop:disable Lint/SuppressedException
-        # Do nothing
-      end
-
-      def runner_context
-        return @runner_context if @runner_context
-
-        raise 'Context was required, but was not set yet'
-      end
-
-      def runner_context=(new_runner_context)
-        @runner_context = new_runner_context
-        @parsed = nil
-      end
-
-      def parsed
-        @parsed ||= ::EacCli::Parser.new(self.class.runner_definition, runner_context.argv).parsed
-      end
-    end
   end
 end
