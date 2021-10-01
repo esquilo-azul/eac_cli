47116beb59a7be61a7db5797e0ac7cf16360fea7
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-10-01 20:36:37 -0300

    EacCli::Config: act as a delegator for sub_node.

--- lib/eac_cli/config.rb
+++ lib/eac_cli/config.rb
@@ -3,16 +3,19 @@
 require 'eac_ruby_utils/core_ext'
 
 module EacCli
-  class Config
+  class Config < ::SimpleDelegator
     require_sub __FILE__
-    attr_reader :sub
 
     def initialize(sub_node)
-      @sub = sub_node
+      super(sub_node)
     end
 
     def entry(path, options = {})
       ::EacCli::Config::Entry.new(self, path, options)
+    end
+
+    def sub
+      __getobj__
     end
   end
 end
